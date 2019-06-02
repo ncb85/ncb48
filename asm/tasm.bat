fd92c8f344a5ee3984ac19692fce653943ba02de
Roman Fulek <roman.fulek@gmail.com>
2019-06-02 15:44:16 +0200

    build simplified

--- asm/tasm.bat
+++ asm/tasm.bat
@@ -4,11 +4,9 @@ SET OLDPATH=%PATH%
 REM assemble source
 SET PATH=%PATH%;..\tasm32
 SET TASMTABS=..\tasm32
-SET TASMOPTS=-48 -o10 -e
+REM SET TASMOPTS=-48 -o10 -e -c -fEA
+SET TASMOPTS=-48 -o10 -e -c -f00
 tasm.exe %HOME%\%1.asm
 SET PATH=%OLDPATH%
 
-REM offset program in hex file for EPROM emulator
-.\..\..\srecord-1.64-win32\srec_cat %1.obj -Intel -offset 0x0A000 -Output %1.hex -Intel -address-length=2 -Output_Block_Size=16
-REM .\..\..\srecord-1.64-win32\srec_cat %1.obj -Intel -offset 0x0E000 -Output %1.hex -Intel -address-length=2 -Output_Block_Size=16
 .\..\..\srecord-1.64-win32\srec_cat %1.obj -Intel -offset 0x0000 -Output %1_000.hex -Intel -address-length=2 -Output_Block_Size=16
