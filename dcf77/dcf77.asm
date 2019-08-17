d1c36c202c7dd9ca604aeb18855419ba06a48157
Roman Fulek <roman.fulek@gmail.com>
2019-08-17 16:47:06 +0200

    processing continued

--- dcf77/dcf77.asm
+++ dcf77/dcf77.asm
@@ -6,6 +6,8 @@
 ; 100ms pulse should set all 4 bits of shift register to ones
 ; 200ms pulse should set all 8 bits of shift register to ones
 			;
+			.MODULE DCF77			; module name (for local _labels)
+			;
 BEGIN		.EQU 400H				; begin address
 TSRADR		.EQU 0780H				; TSR address
 			; monitor routines
@@ -20,13 +22,14 @@ TXBYTE		.EQU 03CH
 			; hw constants
 CRYSTAL		.EQU 4915200			; Hz, timer interrupts 40 times per second
 TICKS		.EQU CRYSTAL/3/5/32/256 ; ticks per second
+LOW_LEN		.EQU TICKS-(TICKS*4/5)+1 ; length of low (second) part of pulse
 			; variables
 PULSE_HIST	.EQU 127				; pulse samples history register
 SECOND		.EQU 126				; seconds
 MINUTE		.EQU 125				; minutes
 HOUR		.EQU 124				; hours
 CURR_STAT	.EQU 123				; current state
-PULS_LEN	.EQU 122				; current pulse length detected
+PULSE_LEN	.EQU 122				; current pulse length detected
 			;
 			; pulse constants
 PULSE_UNKN	.EQU 0FFH				; unknown pulse detected
@@ -36,6 +39,8 @@ PULSE_BEGIN	.EQU 01H				; begin of pulse detected
 PULSE_END	.EQU 02H				; end of pulse detected
 PULSE_59	.EQU 04H				; last second detected
 PULSE_ERR	.EQU 08H				; invalid input detected
+			;
+			#INCLUDE "timer.asm"	; pulse sampling - timer interrupt routine
 			;
 			.ORG BEGIN				; reset vector
 			JMP MAIN				; jump to main routine
--- dcf77/timer.asm
+++ dcf77/timer.asm
@@ -2,6 +2,7 @@
 ; timer interrupt is used for pulse sampling and processing
 ; each 100ms time period is sampled 4x
 			;
+			.MODULE TIMER			; module name (for local _labels)
 			;
 			; Brian Kernighan’s Algorithm to count set bits
 			; Subtraction of 1 from a number toggles all the bits (from right to left)
@@ -12,13 +13,13 @@
 			; The beauty of this solution is the number of times it loops
 			; is equal to the number of set bits in a given integer.
 BCCNSB		MOV R2,#0				; 1  Initialize count: = 0
-			JZ BCCNS3				;   2  If integer n is not zero
-BCCNS2		MOV R3,A				;      (a) Do bitwise & with (n-1) and assign the value back to n
+			JZ _BCCNS3				;   2  If integer n is not zero
+_BCCNS2		MOV R3,A				;      (a) Do bitwise & with (n-1) and assign the value back to n
 			DEC R3					;
 			ANL A,R3				;          n: = n&(n-1)
 			INC R2					;      (b) Increment count by 1
-			JNZ BCCNS2				;      (c) go to step 2
-BCCNS3		MOV A,R2				;   3  Else return count
+			JNZ _BCCNS2				;      (c) go to step 2
+_BCCNS3		MOV A,R2				;   3  Else return count
 			RET						; return number of 1 in shift register
 			;
 			; process pulse - shift register contains last 8 pulses
@@ -32,7 +33,7 @@ PRPULS		MOV R4,A				; back up A
 			ANL A,#0FH				; get latest four samples
 			CALL BCCNSB				; count ones in latest four samples
 			MOV R2,A				; back up A (count of lower nibble ones)
-			MOV R0,#PULS_LEN		; get pulse length variable address to R0
+			MOV R0,#PULSE_LEN		; get pulse length variable address to R0
 			SUBI(2)					; number of ones in current 100ms period less then 2?
 			JC PRPUL1				; yes, we have zero, continue
 			MOV A,R2				; restore count of latest four samples
@@ -40,15 +41,20 @@ PRPULS		MOV R4,A				; back up A
 			JC PRPUE1				; no, unable to decide (noise or in transition so ignore it)
 PRPUH1		MOV A,R4				; restore count of previous four samples
 			SUBI(2)					; number of ones in previous 100ms period less then 2?
-			JC PRPUH2				; previous was low, begin of new pulse (second)
+			JC PRPUH2				; previous was low, beginning of new pulse (second)
 			INC @R0					; previous was high, pulse still unfinished, increment length
 			RET
-PRPUH2		MOV @R0,#PULS_LEN		; check low pulse length at least 800ms
-			MOV A,@R0				; get pulse length
-			SUBI(TICKS-TICKS*4/5+1)	; zero level present longer then 800ms? (completed pulse)
+PRPUH2		MOV R0,#CURR_STAT		; get address of current state variable
+			MOV A,@R0				; get state (PULSE_ZERO or PULSE_ONE)
+			ANL A,#PULSE_ONE		; ignore other bits
+			RR A					; divide by 2 (resulting in 4 or 0)
+			MOV R0,#PULSE_LEN		; check low pulse length at least 800ms
+			XCH A,@R0				; exchange values
+			ADD A,@R0
+			SUBI(LOW_LEN)			; zero level present longer then 800ms? (completed pulse)
 			JC	PRPUE1				; invalid pulse, low state was too short
 			MOV @R0,#0				; start measuring pulse width
-			MOV @R0,#CURR_STAT		; get address of current state variable
+			MOV R0,#CURR_STAT		; get address of current state variable
 			MOV @R0,#PULSE_BEGIN	; set pulse begin
 			CLR F1					; set pulse valid flag F1
 			CPL F1					; pulse valid, start decoding
@@ -56,18 +62,25 @@ PRPUH2		MOV @R0,#PULS_LEN		; check low pulse length at least 800ms
 PRPUL1		INC @R0					; increment length variable address
 			MOV A,R4				; restore count of previous four samples
 			SUBI(2)					; number of ones in previous 100ms period less then 2?
-			JC PRPUL2				; yes, still no new pulse detected
+			JC PRPUL4				; yes, still no new pulse detected
 			MOV A,R2				; restore count of previous four samples
 			SUBI(3)					; is count above 2 (3 or 4)?
 			JC PRPUE1				; no, unable to decide (noise or in transition so ignore it)
 			; fall through			  previous was high, we have end of pulse
-			MOV @R0,#CURR_STAT		; get address of current state variable
-			MOV @R0,#PULSE_END		; set pulse end
-			TODO evaluate pulse length, set ZERO or ONE
-			PULSE_ZERO
-			PULSE_ONE
+			MOV R0,#CURR_STAT		; get address of current state variable
+			MOV @R0,#PULSE_END		; set pulse end, evaluate it
+			MOV R0,#PULSE_LEN		; get pulse length variable address to R0
+			MOV A,@R0				; get pulse length
+			SUBI(6)					; is count above 5?
+			JNC PRPUL2				; yes, it is long pulse e.g. PULSE_ONE
+			MOV A,#PULSE_ZERO		; no, PULSE_ZERO
+			JMP PRPUL3
+PRPUL2		MOV A,#PULSE_ONE		; PULSE_ONE
+PRPUL3		MOV R0,#CURR_STAT		; get address of current state variable
+			ORL A,@R0				; combine values
+			XCH A,@R0				; exchange values (set CURR_STAT)
 			RET
-PRPUL2		MOV R0,#PULS_LEN		; get pulse length variable address to R0
+PRPUL4		MOV R0,#PULSE_LEN		; get pulse length variable address to R0
 			MOV A,@R0				; get pulse length
 			SUBI(TICKS)				; zero level present longer then second? (sec.59)
 			JNC	PRPUL9				; yes, it is second nr.59
