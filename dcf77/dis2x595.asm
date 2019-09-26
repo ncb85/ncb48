79e669968ddf5272f5bc8e9aa29a9ec2afe78f1e
Roman Fulek <roman.fulek@gmail.com>
2019-09-26 21:37:41 +0200

    multiplexed display code cleaned

--- dcf77/dis2x595.asm
+++ dcf77/dis2x595.asm
@@ -14,6 +14,44 @@ _DIB2		ORL P1,#CLOCK_PIN		; set clock pin
 			DJNZ R2,_DIB1			; shift out all segments
 			RET
 			;
+			; display one digit according to current position on display
+DISP_TIME	MOV R0,#POSITION		; get address of display position variable
+_DISP_T1	MOV A,@R0				; move position to A
+			MOV R4,#01H				; left display digit
+			JNZ _DISP_T2			; first position on display
+			MOV R1,#HOUR			; get address of hour variable
+			MOV A,@R1				; fetch hours
+			ANL A,#0F0H				; less than 10?
+			JNZ _DISP_T5			; no, continue displaying
+			INC @R0					; skip leading zero
+			JMP _DISP_T1			; continue displaying
+_DISP_T2	MOV R2,A				; move position to R2
+			SUBI(2)					; third or fourth column?
+			JC _DISP_T3				; no, jump
+			INC R2					; skip one display position
+			MOV A,R2				; move position to A
+			SUBI(5)					; fifth or sixth column?
+			JC _DISP_T3				; no, jump
+			INC R2					; skip one display position
+_DISP_T3	MOV A,R4				; get column to A
+_DISP_T4	RL A					; shift column
+			DJNZ R2,_DISP_T4		; decrement pos and loop
+			MOV R4,A				; set column
+_DISP_T5	MOV A,@R0				; get position to A
+			RR A					; divide by 2
+			ADD A,#HOUR				; add address of hour variable
+			MOV R0,A				; result to R0
+			MOV A,@R0				; get hour/minute/second
+			MOV R2,A				; backup to R2
+			MOV R0,#POSITION		; get address of display position variable
+			MOV A,@R0				; move position to A
+			RRC A					; bit 0 to CY
+			MOV A,R2				; get value from R2
+			JC _DISP_T6				; lower nibble
+			SWAP A					; swap nibbles
+_DISP_T6	ANL A,#0FH				; mask out higher BCD number
+			;JMP DISP_BCD			; display at computed position
+			; jump optimised out
 			; diplay BCD encoded number, R4 position on display, A - bcd number
 DISP_BCD	MOV R3,A				; move param to R3
 			MOV A,R4				; get position
@@ -26,43 +64,6 @@ DISP_BCD	MOV R3,A				; move param to R3
 			ANL P1,#~LATCH_PIN		; clear latch pin
 			RET
 			;
-			; display one digit according to current position on display
-DISP_TIME	MOV R0,#POSITION		; get address of display position variable
-			MOV A,@R0				; move position to A
-			MOV R4,#01H				; left display digit
-			JZ _DISP_T6				; first position on display
-			MOV R2,A				; move position to R2
-			SUBI(2)					; third or fourth column?
-			JC _DISP_T2				; no, jump
-			INC R2					; skip one display position
-_DISP_T1	MOV A,R2				; move position to A
-			SUBI(5)					; fifth or sixth column?
-			JC _DISP_T2				; no, jump
-			INC R2					; skip one display position
-_DISP_T2	MOV A,R4				; get column to A
-_DISP_T3	RL A					; shift column
-			DJNZ R2,_DISP_T3		; decrement pos and loop
-			MOV R4,A				; set column
-_DISP_T4	MOV A,@R0				; get position to A
-			RR A					; divide by 2
-			ADD A,#HOUR				; add address of hour variable
-			MOV R0,A				; result to R0
-			MOV A,@R0				; get hour/minute/second
-			MOV R2,A				; backup to R2
-			MOV R0,#POSITION		; get address of display position variable
-			MOV A,@R0				; move position to A
-			RRC A					; bit 0 to CY
-			MOV A,R2				; get value from R2
-			JC _DISP_T5				; lower nibble
-			SWAP A					; swap nibbles
-_DISP_T5	ANL A,#0FH				; mask out higher BCD number
-			JMP DISP_BCD			; display at computed position
-_DISP_T6	MOV R1,#HOUR			; get address of hour variable
-			MOV A,@R1				; fetch hours
-			ANL A,#0F0H				; less than 10?
-			JNZ _DISP_T4			; no, continue displaying
-			INC @R0					; skip leading zero
-			JMP DISP_TIME+2			; continue displaying
 			;
 			; seven segment display table
 DIGIT		.DB ~03FH				; 0
@@ -75,6 +76,3 @@ DIGIT		.DB ~03FH				; 0
 			.DB ~007H				; 7
 			.DB ~07FH				; 8
 			.DB ~06FH				; 9
-			.DB ~00H				; space [10]
-			.DB ~80H				; . [11]
-			.DB ~08H				; _ [12]
