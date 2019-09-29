a77a22a4845e57e38e8ee16e1f908204085f909b
Roman Fulek <roman.fulek@gmail.com>
2019-09-29 19:16:57 +0200

    debug changes R1 reg

--- dcf77/debug.asm
+++ dcf77/debug.asm
@@ -12,46 +12,46 @@ CTRUART		.EQU 09H				; UART control register
 BACK_A		.EQU 0FFH				; register backup area FF-F9 in RIOT
 			;
 			; 8251A initialisation, according to datasheet (3x 00h + RESET 040h)
-LOGINIT		MOV R0,#CTRUART			; 8251A control register address
+LOGINIT		MOV R1,#CTRUART			; 8251A control register address
 			CLR A					; clear A
-			MOVX @R0,A				; 00H to control regiter
-			MOVX @R0,A				; 00H to control regiter
-			MOVX @R0,A				; 3x 00H to control regiter
+			MOVX @R1,A				; 00H to control regiter
+			MOVX @R1,A				; 00H to control regiter
+			MOVX @R1,A				; 3x 00H to control regiter
 			MOV A,#40H				; reset 40h
-			MOVX @R0,A				; register write
+			MOVX @R1,A				; register write
 			MOV A,#4EH				; MODE: no parity, 8 databits, 1 stop bit, 16x
-			MOVX @R0,A				; register write
+			MOVX @R1,A				; register write
 			MOV A,#15H				; COMMAND: enable receive and transmit
-			MOVX @R0,A				; register write
+			MOVX @R1,A				; register write
 			RET
 			;
-			; log with registers preserved - R0 is changed though
+			; log with registers preserved - R1 is changed though
 LOGACC		ANL P2,#~IOM8155		; select 8155 RAM(deny UART, RIOT)
-			MOV R0,#BACK_A			; backup RAM address
-			MOVX @R0,A				; backup A
+			MOV R1,#BACK_A			; backup RAM address
+			MOVX @R1,A				; backup A
 			MOV A,R7				; move R7 to A
-			DEC R0
-			MOVX @R0,A				; backup R7
-			DEC R0
+			DEC R1
+			MOVX @R1,A				; backup R7
+			DEC R1
 			MOV A,R6
-			MOVX @R0,A				; backup R6
-			DEC R0
+			MOVX @R1,A				; backup R6
+			DEC R1
 			MOV A,R5
-			MOVX @R0,A				; backup R5
-			DEC R0
+			MOVX @R1,A				; backup R5
+			DEC R1
 			MOV A,R4
-			MOVX @R0,A				; backup R4
-			DEC R0
+			MOVX @R1,A				; backup R4
+			DEC R1
 			MOV A,R3
-			MOVX @R0,A				; backup R3
-			DEC R0
+			MOVX @R1,A				; backup R3
+			DEC R1
 			MOV A,R2
-			MOVX @R0,A				; backup R2
-			DEC R0
-			MOV A,R1
-			MOVX @R0,A				; backup R1
-			MOV R0,#BACK_A			; address of A
-			MOVX A,@R0				; restore A
+			MOVX @R1,A				; backup R2
+			DEC R1
+			MOV A,R0
+			MOVX @R1,A				; backup R0
+			MOV R1,#BACK_A			; address of A
+			MOVX A,@R1				; restore A
 			ORL P2,#IOM8155			; deselect 8155 RAM(allow UART, RIOT)
 			;
 TXBYTE		MOV R3,A				; backup A to R3
@@ -67,13 +67,13 @@ TXBYTE		MOV R3,A				; backup A to R3
 			JMP _TXBY4				; jump over
 _TXBY3		ADD A,#'A'-10			; it is alpha digit A..F add 'A'
 _TXBY4		MOV R2,A				; move to R2
-	 		MOV R0,#CTRUART			; 8251A control/status reg
-_TXC1		MOVX A,@R0				; status register to A
+	 		MOV R1,#CTRUART			; 8251A control/status reg
+_TXC1		MOVX A,@R1				; status register to A
 			JB0 _TXC2				; bit 0 TxRDY - buffer empty - proceed sending
 			JMP _TXC1				; wait more
-_TXC2		DEC R0					; 8251A data reg
+_TXC2		DEC R1					; 8251A data reg
 			MOV A,R2				; move char to A
-			MOVX @R0,A				; send it
+			MOVX @R1,A				; send it
 			; lower nibble
 			MOV A,R3				; restore input value
 TXNIBB		MOV R4,#10				; compare value
@@ -86,68 +86,68 @@ TXNIBB		MOV R4,#10				; compare value
 			JMP TXBY4				; jump over
 TXBY3		ADD A,#'A'-10			; it is alpha digit A..F add 'A'
 TXBY4		MOV R2,A				; move to R2
-	 		MOV R0,#CTRUART			; 8251A control/status reg
-TXC1		MOVX A,@R0				; status register to A
+	 		MOV R1,#CTRUART			; 8251A control/status reg
+TXC1		MOVX A,@R1				; status register to A
 			JB0 TXC2				; bit 0 TxRDY - buffer empty - proceed sending
 			JMP TXC1				; wait more
-TXC2		DEC R0					; 8251A data reg
+TXC2		DEC R1					; 8251A data reg
 			MOV A,R2				; move char to A
-			MOVX @R0,A				; send it
+			MOVX @R1,A				; send it
 			; restore registers
 			ANL P2,#~IOM8155		; select 8155 RAM(deny UART, RIOT)
-			MOV R0,#BACK_A-7		; backup RAM address
-			MOVX A,@R0				; restore R1
-			MOV R1,A				; move A to R1
-			INC R0
-			MOVX A,@R0				; restore R2
+			MOV R1,#BACK_A-7		; backup RAM address
+			MOVX A,@R1				; restore R0
+			MOV R0,A				; move A to R0
+			INC R1
+			MOVX A,@R1				; restore R2
 			MOV R2,A				; move A to R2
-			INC R0
-			MOVX A,@R0				; restore R3
+			INC R1
+			MOVX A,@R1				; restore R3
 			MOV R3,A
-			INC R0
-			MOVX A,@R0				; restore R4
+			INC R1
+			MOVX A,@R1				; restore R4
 			MOV R4,A
-			INC R0
-			MOVX A,@R0				; restore R5
+			INC R1
+			MOVX A,@R1				; restore R5
 			MOV R5,A
-			INC R0
-			MOVX A,@R0				; restore R6
+			INC R1
+			MOVX A,@R1				; restore R6
 			MOV R6,A
-			INC R0
-			MOVX A,@R0				; restore R7
+			INC R1
+			MOVX A,@R1				; restore R7
 			MOV R7,A				; move A to R7
-			INC R0					; A backup address
-			MOVX A,@R0				; restore A
+			INC R1					; A backup address
+			MOVX A,@R1				; restore A
 			ORL P2,#IOM8155			; deselect 8155 RAM(allow UART, RIOT)
 			RET
 			;
-			; logs char in A with registers preserved - R0 is changed though
+			; logs char in A with registers preserved - R1 is changed though
 LOGIMD		ANL P2,#~IOM8155		; select 8155 RAM(deny UART, RIOT)
-			MOV R0,#BACK_A			; backup RAM address
-			MOVX @R0,A				; backup A
-			DEC R0
+			MOV R1,#BACK_A			; backup RAM address
+			MOVX @R1,A				; backup A
+			DEC R1
 			MOV A,R2				; move R2 to A
-			MOVX @R0,A				; backup R2
-			MOV R0,#BACK_A			; backup RAM address
-			MOVX A,@R0				; restore A
+			MOVX @R1,A				; backup R2
+			MOV R1,#BACK_A			; backup RAM address
+			MOVX A,@R1				; restore A
 			ORL P2,#IOM8155			; deselect 8155 RAM(allow UART, RIOT)
 			;
 			MOV R2,A				; move to R2
-	 		MOV R0,#CTRUART			; 8251A control/status reg
-_LGI1		MOVX A,@R0				; status register to A
+	 		MOV R1,#CTRUART			; 8251A control/status reg
+_LGI1		MOVX A,@R1				; status register to A
 			JB0 _LGI2				; bit 0 TxRDY - buffer empty - proceed sending
 			JMP _LGI1				; wait more
-_LGI2		DEC R0					; 8251A data reg
+_LGI2		DEC R1					; 8251A data reg
 			MOV A,R2				; move char to A
-			MOVX @R0,A				; send it
+			MOVX @R1,A				; send it
 			;
 			; restore registers
 			ANL P2,#~IOM8155		; select 8155 RAM(deny UART, RIOT)
-			MOV R0,#BACK_A-1		; backup RAM address
-			MOVX A,@R0				; restore R2
+			MOV R1,#BACK_A-1		; backup RAM address
+			MOVX A,@R1				; restore R2
 			MOV R2,A				; move A to R2
-			INC R0
-			MOVX A,@R0				; restore A
+			INC R1
+			MOVX A,@R1				; restore A
 			ORL P2,#IOM8155			; deselect 8155 RAM(allow UART, RIOT)
 			RET
 			;
